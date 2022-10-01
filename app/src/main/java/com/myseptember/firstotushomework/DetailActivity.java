09393427501acc1e9fb7921d9906852fd9a56eef
Alina Vagner <ali.vag977@gmail.com>
2022-10-01 11:59:55 +0300

    Добавлена анимация, выдвижная панель сбоку и меню, О программе и перемещена кнопка "Пригласить друга"

--- app/src/main/java/com/myseptember/firstotushomework/DetailActivity.java
+++ app/src/main/java/com/myseptember/firstotushomework/DetailActivity.java
@@ -1,18 +1,27 @@
 package com.myseptember.firstotushomework;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class DetailActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class DetailActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private static final String TAG = "myLogs";
     AboutDetails aboutDetails;
@@ -25,6 +34,8 @@ public class DetailActivity extends AppCompatActivity {
     EditText comment;
 
     String name;
+    DrawerLayout drawer;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +65,64 @@ public class DetailActivity extends AppCompatActivity {
         btnBack.setOnClickListener(this::onClick);
 
         like.setChecked(getIntent().getBooleanExtra("Like",false));
+
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);;
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ImageView view = findViewById(R.id.imageview_animation_list);
+        view.setImageResource(R.drawable.frame_anim);
+        ((AnimationDrawable) view.getDrawable()).start();
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.about) {
+            Toast.makeText(getApplicationContext(), "Это новое приложение", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.mainScreen) {
+            drawer.closeDrawer(GravityCompat.START);
+            Intent data = new Intent();
+            data.putExtra(MainActivity.ACCESS_MESSAGE, "Activity Closed");
+            data.putExtra("Like", like.isChecked());
+            Log.println(Log.INFO,TAG,"Для фильма "+name+" наличие лайка: " + String.valueOf(like.isChecked()));
+            data.putExtra("Name", name);
+            data.putExtra("Comment", comment.getText().toString());
+            Log.println(Log.INFO,TAG,"Для фильма "+name+" комментарий: " + comment.getText().toString());
+            setResult(RESULT_OK, data);
+            finish();
+            return true;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_layout, menu);
+        return true;
     }
 
     public void onClick(View v) {
--- app/src/main/java/com/myseptember/firstotushomework/MainActivity.java
+++ app/src/main/java/com/myseptember/firstotushomework/MainActivity.java
@@ -5,28 +5,35 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.Navigation;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
+import com.google.android.material.navigation.NavigationView;
+
 import java.util.List;
 
 import butterknife.BindViews;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,NavigationView.OnNavigationItemSelectedListener {
 
     static final String ACCESS_MESSAGE="ACCESS_MESSAGE";
 
@@ -42,8 +49,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @BindViews({R.id.comment,R.id.comment2,R.id.comment3,R.id.comment4,R.id.comment5})
     List<TextView> comments;
 
-
-    Button btnInvite;
+    DrawerLayout drawer;
+    NavigationView navigationView;
 
     ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
@@ -75,11 +82,74 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        btnInvite = (Button) findViewById(R.id.btnInvite);
-        btnInvite.setOnClickListener(this);
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);;
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    @OnClick({ R.id.btnDetails,R.id.btnDetails2,R.id.btnDetails3,R.id.btnDetails4,R.id.btnDetails5,R.id.btnInvite })
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.about) {
+            Toast.makeText(getApplicationContext(), "Это новое приложение", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.mainScreen) {
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_layout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id){
+            case R.id.action_close :
+                finish();
+                return true;
+            case R.id.action_invite:
+                String textMessage = "Share with friends";
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, textMessage);
+                sendIntent.setType("text/plain");
+                String title = getResources().getString(R.string.chooser_title);
+                Intent chooser = Intent.createChooser(sendIntent, title);
+                if (sendIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(chooser);
+                }
+                return true;
+        }
+        //headerView.setText(item.getTitle());
+        return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick({ R.id.btnDetails,R.id.btnDetails2,R.id.btnDetails3,R.id.btnDetails4,R.id.btnDetails5})
     public void onClick(View v) {
         Intent intent = new Intent(MainActivity.this, DetailActivity.class);
         switch (v.getId()) {
@@ -112,18 +182,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 intent.putExtra("Det",new AboutDetails("jojo5","jojo5","jojo5"));
                 intent.putExtra("Like", checkLikeBoxes.get(4).isChecked());
                 mStartForResult.launch(intent);
-                break;
-            case R.id.btnInvite:
-                String textMessage = "Share with friends";
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, textMessage);
-                sendIntent.setType("text/plain");
-                String title = getResources().getString(R.string.chooser_title);
-                Intent chooser = Intent.createChooser(sendIntent, title);
-                if (sendIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(chooser);
-                }
                 break;
             default:
                 break;
